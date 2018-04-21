09c30ff20bc80b4d7f01666ed8b34047f23d243a
vromankovs <v.romankovs@gmail.com>
2018-04-21 19:34:02 +0300

    new changes

---
+++ src/lv/akurss/github_test/Main.java
@@ -0,0 +1,13 @@
+package lv.akurss.github_test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person newPerson = new Person("Joe");
+
+        System.out.printf("Hello %s", newPerson.getName());
+
+    }
+
+}
